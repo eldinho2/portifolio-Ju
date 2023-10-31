50814c92eeaa96e09e37fa2df69d01dcb83d056e
Eldinho2 <viniciusfod@gmail.com>
2023-10-31 18:17:56 -0300

    preconceitos

---
+++ app/(pages)/PreconceitosEAMatematica/page.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+
+const Coletivo = () => {
+  return (
+    <main className="flex font-mon p-10 justify-center items-center">
+      <div>
+        <div>
+          <h1 className="font-play text-6xl font-semibold py-3">
+            Preconceitos e a Matemática
+          </h1>
+          <div className="flex gap-5 text-lg font-semibold">
+            <div>
+              <h1 className="text-center">Transfobia</h1>
+              <div className="bg-[#B8CDDB] p-6 w-64 rounded-xl">
+                <h1>aaaaaaaaaaaa</h1>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Magni, rem harum asperiores eveniet quo deleniti, modi
+                  sapiente beatae dolore ducimus incidunt dolor expedita.
+                  Consequatur, eaque? Id voluptatum ipsam alias earum.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h1 className="text-center">Racismo</h1>
+
+              <div className="bg-[#F7BB97] p-6 w-64 rounded-xl">
+                <h1>aaaaaaaaaaaa</h1>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Magni, rem harum asperiores eveniet quo deleniti, modi
+                  sapiente beatae dolore ducimus incidunt dolor expedita.
+                  Consequatur, eaque? Id voluptatum ipsam alias earum.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h1 className="text-center">Ditadura</h1>
+
+              <div className="bg-[#FFD2CC] p-6 w-64 rounded-xl">
+                <h1>aaaaaaaaaaaa</h1>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Magni, rem harum asperiores eveniet quo deleniti, modi
+                  sapiente beatae dolore ducimus incidunt dolor expedita.
+                  Consequatur, eaque? Id voluptatum ipsam alias earum.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Coletivo;
